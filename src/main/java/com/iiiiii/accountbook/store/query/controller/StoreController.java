1a3e580ce0b65e69474dc4a586b4a24555a4c2b7
yanghyeonjin <guswls7081@gmail.com>
2024-08-26 16:55:24 +0900

    FEATURE: findStoreById 완료

--- src/main/java/com/iiiiii/accountbook/store/query/controller/StoreController.java
+++ src/main/java/com/iiiiii/accountbook/store/query/controller/StoreController.java
@@ -3,11 +3,15 @@ package com.iiiiii.accountbook.store.query.controller;
 import com.iiiiii.accountbook.store.query.dto.StoreDTO;
 import com.iiiiii.accountbook.store.query.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/stores")
 public class StoreController {
 
     private final StoreService storeService;
@@ -17,7 +21,13 @@ public class StoreController {
         this.storeService = storeService;
     }
 
+    @GetMapping("/")
     public List<StoreDTO> findAllStores() {
         return storeService.findAllStores();
+    }
+
+    @GetMapping("/{storeCode}")
+    public StoreDTO findStoreById(@PathVariable int storeCode) {
+        return storeService.findStoreById(storeCode);
     }
 }
--- src/test/java/com/iiiiii/accountbook/store/query/service/StoreServiceTests.java
+++ src/test/java/com/iiiiii/accountbook/store/query/service/StoreServiceTests.java
@@ -2,9 +2,14 @@ package com.iiiiii.accountbook.store.query.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,9 +20,24 @@ class StoreServiceTests {
     @Autowired
     private StoreService storeService;
 
-    @Test
     @DisplayName("가게 조회 테스트")
+    @Test
     public void testFindAllStores() {
         assertDoesNotThrow(() -> storeService.findAllStores());
+    }
+
+    private static Stream<Arguments> storeCodeSource() {
+        return Stream.of(
+                Arguments.of(1),
+                Arguments.of(2),
+                Arguments.of(10)
+        );
+    }
+
+    @DisplayName("가게 ID로 가게 1개 조회 테스트")
+    @ParameterizedTest
+    @MethodSource("storeCodeSource")
+    public void testFindStoreById(int storeCode) {
+        assertDoesNotThrow(() -> storeService.findStoreById(storeCode));
     }
 }
