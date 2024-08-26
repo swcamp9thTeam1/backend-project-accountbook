3469754f9dc9cedc08631f00b62e15b7df592952
yanghyeonjin <guswls7081@gmail.com>
2024-08-26 16:23:36 +0900

    FEATURE: @Controller -> @RestController로 변경

--- src/main/java/com/iiiiii/accountbook/store/query/controller/StoreController.java
+++ src/main/java/com/iiiiii/accountbook/store/query/controller/StoreController.java
@@ -3,11 +3,11 @@ package com.iiiiii.accountbook.store.query.controller;
 import com.iiiiii.accountbook.store.query.dto.StoreDTO;
 import com.iiiiii.accountbook.store.query.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class StoreController {
 
     private final StoreService storeService;
