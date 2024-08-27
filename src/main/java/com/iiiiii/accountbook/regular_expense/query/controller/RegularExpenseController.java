3690b4e4250313705f3f38b40964f43cee3887da
iwillbfine <60764768+iwillbfine@users.noreply.github.com>
2024-08-27 18:12:07 +0900

    고정 지출 완료

---
+++ src/main/java/com/iiiiii/accountbook/regular_expense/query/controller/RegularExpenseController.java
@@ -0,0 +1,67 @@
+package com.iiiiii.accountbook.regular_expense.query.controller;
+
+import com.iiiiii.accountbook.common.ResponseMessage;
+import com.iiiiii.accountbook.common.ResponseStatusText;
+import com.iiiiii.accountbook.regular_expense.query.dto.RegularExpenseDTO;
+import com.iiiiii.accountbook.regular_expense.query.dto.ResponseRegularExpenseDTO;
+import com.iiiiii.accountbook.regular_expense.query.service.RegularExpenseService;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController("queryController")
+@RequestMapping("/regular-expenses")
+@MapperScan("com.iiiiii.accountbook.regular_expense.query")
+public class RegularExpenseController {
+    private RegularExpenseService regularExpenseService;
+
+    @Autowired
+    public RegularExpenseController(RegularExpenseService regularExpenseService) {
+        this.regularExpenseService = regularExpenseService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseMessage> findAllRegularExpenses() {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "json"
+                , Charset.forName("UTF-8")));
+
+        List<RegularExpenseDTO> regularExpenses = regularExpenseService.findAllRegularExpenses();
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("regularExpenses", regularExpenses);
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(ResponseStatusText.OK,"조회 성공!", responseMap));
+    }
+
+    @GetMapping("/{memberCode}")
+    public ResponseEntity<ResponseMessage> findOneMemberRegularExpenses(@PathVariable("memberCode") int memberCode) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "json"
+                , Charset.forName("UTF-8")));
+
+        List<ResponseRegularExpenseDTO> regularExpenses = regularExpenseService.findOneMemberRegularExpenses(memberCode);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("memberCode", memberCode);
+        responseMap.put("regularExpenses", regularExpenses);
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(ResponseStatusText.OK, "조회 성공!", responseMap));
+    }
+}
---
+++ src/main/java/com/iiiiii/accountbook/regular_expense/query/repository/RegularExpenseMapper.java
@@ -0,0 +1,15 @@
+package com.iiiiii.accountbook.regular_expense.query.repository;
+
+import com.iiiiii.accountbook.regular_expense.query.dto.RegularExpenseDTO;
+import com.iiiiii.accountbook.regular_expense.query.dto.ResponseRegularExpenseDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface RegularExpenseMapper {
+
+    List<RegularExpenseDTO> findAllRegularExpenses();
+
+    List<ResponseRegularExpenseDTO> findOneMemberRegularExpenses(int memberCode);
+}
