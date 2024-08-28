3ccca0419883ff9dd32edb6cc6cedc6cba802854
Rohdamin <rohdamin3@gmail.com>
2024-08-28 16:47:07 +0900

    FIX: 가계부 코멘트 DTO에 코멘트 코드 추가

--- src/main/java/com/iiiiii/accountbook/accbook/query/dto/AccCommentDTO.java
+++ src/main/java/com/iiiiii/accountbook/accbook/query/dto/AccCommentDTO.java
@@ -10,6 +10,7 @@ import lombok.*;
 public class AccCommentDTO {
     private String detail;
     private String createdAt;
+    private Integer commentCode;    // 댓글 코드
     private Integer parentCode;
     private Integer memberCode;     // 댓글 작성자
 }
