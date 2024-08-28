9d24ee02510920b529586cc08e0614f4d12a80b8
yanghyeonjin <guswls7081@gmail.com>
2024-08-28 20:13:31 +0900

    FIX: 착한가격업소 등록 테스트코드 수정
    - 미사용변수 사용처리, 미사용 import 제거

--- src/test/java/com/iiiiii/accountbook/store/command/application/service/StoreServiceTests.java
+++ src/test/java/com/iiiiii/accountbook/store/command/application/service/StoreServiceTests.java
@@ -1,19 +1,14 @@
 package com.iiiiii.accountbook.store.command.application.service;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,25 +19,18 @@ class StoreServiceTests {
     @Autowired
     private StoreService storeService;
 
-    private static Stream<Arguments> goodStoreFile() {
-        return Stream.of(
-                Arguments.of(new File("src/test/resources/bsshList_20240827123500.xls"))
-        );
-    }
-
     @DisplayName("착한가격업소 등록 테스트 (엑셀 파일 사용)")
-    @ParameterizedTest
-    @MethodSource("goodStoreFile")
-    public void testRegisterGoodStore(File goodStoreFile) {
+    @Test
+    public void testRegisterGoodStore() {
 
         // given
         MockMultipartFile mockMultipartFile = null;
 
         String fileName = "bsshList_20240827123500";
         String contentType = "xls";
-        String filePath = "src/test/resources/excel/bsshList_20240827123500.xls";
+        String filePath = "src/test/resources/bsshList_20240827123500.xls";
 
-        try (FileInputStream fileInputStream = new FileInputStream(goodStoreFile)) {
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
             mockMultipartFile = new MockMultipartFile(
                     fileName,
                     fileName + "." + contentType,
