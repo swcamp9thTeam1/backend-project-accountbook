e1435e54b315e45e28de41016204b1b9e062fb57
yanghyeonjin <guswls7081@gmail.com>
2024-08-29 17:02:38 +0900

    STYLE: 주석 추가

--- src/main/java/com/iiiiii/accountbook/common/ResponseError.java
+++ src/main/java/com/iiiiii/accountbook/common/ResponseError.java
@@ -13,6 +13,6 @@ public class ResponseError extends ResponseCommon {
 
     public ResponseError(Exception e, String errMessage) {
         super(ResponseStatusText.FAIL, errMessage);
-        this.errCode = e.getClass().getSimpleName();
+        this.errCode = e.getClass().getSimpleName();    // 예외클래스명을 errCode로 내보내기
     }
 }
