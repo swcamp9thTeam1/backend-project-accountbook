a4c5ec80bcb120c43274f9422da2b9171e590776
yncayn <julieche3@gmail.com>
2024-08-29 19:11:29 +0900

    Revert "Revert "리뷰 추가 command 작성""
    This reverts commit 5faeb83152f46ead8e86f5b7d64207e7cdb6a7bf.

---
+++ src/main/java/com/iiiiii/accountbook/review/command/application/service/StoreReviewService.java
@@ -0,0 +1,37 @@
+package com.iiiiii.accountbook.review.command.application.service;
+
+import com.iiiiii.accountbook.review.command.domain.aggregate.entity.StoreReview;
+import com.iiiiii.accountbook.review.command.domain.repository.StoreReviewRepository;
+import com.iiiiii.accountbook.review.command.domain.aggregate.dto.StoreReviewDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("StoreReviewServiceCommand")
+@Slf4j
+public class StoreReviewService {
+
+    private StoreReviewRepository storeReviewRepository;
+
+    @Autowired
+    public StoreReviewService(StoreReviewRepository storeReviewRepository) {
+        this.storeReviewRepository = storeReviewRepository;
+    }
+
+
+    // 주석. 리뷰 서비스 - 리뷰 등록 메소드
+    public StoreReview registStoreReview(StoreReviewDTO newStoreReview) {
+        StoreReview storeReview = new StoreReview();
+        storeReview.setCreatedAt(newStoreReview.getCreatedAt());
+        storeReview.setVisitors(newStoreReview.getVisitors());
+        storeReview.setTotalExpense(newStoreReview.getTotalExpense());
+        storeReview.setOneLineReview(newStoreReview.getOneLineReview());
+        storeReview.setMemberCode(newStoreReview.getMemberCode());
+        storeReview.setStoreCode(newStoreReview.getStoreCode());
+
+        storeReviewRepository.save(storeReview);
+        return storeReview;
+    }
+
+
+}
