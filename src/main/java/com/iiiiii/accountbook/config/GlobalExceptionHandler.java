c2960275188052137e06b21e41f637d837fc81c2
yanghyeonjin <guswls7081@gmail.com>
2024-08-29 17:03:13 +0900

    REFACTOR: 예외 구분하여 response status code를 다르게 내보내도록 수정

--- src/main/java/com/iiiiii/accountbook/config/GlobalExceptionHandler.java
+++ src/main/java/com/iiiiii/accountbook/config/GlobalExceptionHandler.java
@@ -5,23 +5,44 @@ import com.iiiiii.accountbook.exception.NotValidRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    // 500 에러에 대한 공통 처리
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ResponseError> internalServerErrorHandler(Exception e) {
-        e.printStackTrace();
+    // status code 400
+    @ExceptionHandler(NotValidRequestException.class)
+    public ResponseEntity<ResponseError> handleBadRequest(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ResponseError(e, e.getMessage()));
+    }
 
-        return new ResponseEntity<>(
-                new ResponseError(e, e.getMessage()),
-                HttpStatus.INTERNAL_SERVER_ERROR
-        );
+    // status code 404
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ResponseError> handleNotFound(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseError(e, e.getMessage()));
+    }
+
+    // status code 405
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ResponseError> handleRequestMethodNotSupported(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ResponseError(e, e.getMessage()));
+    }
+
+    // status code 500 (나머지 예외에 대해 처리)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseError> handleOthers(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseError(e, e.getMessage()));
     }
 }
