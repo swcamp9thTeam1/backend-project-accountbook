db3cc8122ed36953c07ba7fedaf2a605e88b4266
yncayn <julieche3@gmail.com>
2024-08-29 19:08:56 +0900

    변경사항 커밋 - 수정중

--- src/main/java/com/iiiiii/accountbook/review/query/dto/StoreReviewDTO.java
+++ src/main/java/com/iiiiii/accountbook/review/query/dto/StoreReviewDTO.java
@@ -3,82 +3,80 @@ package com.iiiiii.accountbook.review.query.dto;
 import java.time.LocalDateTime;
 
 public class StoreReviewDTO {
-    private int code ;
-    private LocalDateTime createdAt;
-    private int visitors;
-    private long totalExpense;
-    private String oneLineReview;
-    private int memberCode;
-    private int storeCode;
-
-
-    public StoreReviewDTO() {
-    }
-
-    public StoreReviewDTO(int code, LocalDateTime createdAt, int visitors, long totalExpens,
-                          String oneLineReview, int memberCode, int storeCode){
-        this.code = code;
-        this.createdAt = createdAt;
-        this.visitors = visitors;
-        this.totalExpense = totalExpense;
-        this.oneLineReview = oneLineReview;
-        this.memberCode = memberCode;
-        this.storeCode = storeCode;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public int getVisitors() {
-        return visitors;
-    }
-
-    public void setVisitors(int visitors) {
-        this.visitors = visitors;
-    }
-
-    public long getTotalExpense() {
-        return totalExpense;
-    }
-
-    public void setTotalExpense(long totalExpense) {
-        this.totalExpense = totalExpense;
-    }
-
-    public String getOneLineReview() {
-        return oneLineReview;
-    }
-
-    public void setOneLineReview(String oneLineReview) {
-        this.oneLineReview = oneLineReview;
-    }
-
-    public int getMemberCode() {
-        return memberCode;
-    }
-
-    public void setMemberCode(int memberCode) {
-        this.memberCode = memberCode;
-    }
-
-    public int getStoreCode() {
-        return storeCode;
-    }
-
-    public void setStoreCode(int storeCode) {
-        this.storeCode = storeCode;
-    }
+//    private int code;
+//    private LocalDateTime createdAt;
+//    private int visitors;
+//    private long totalExpense;
+//    private String oneLineReview;
+//    private int memberCode;
+//    private int storeCode;
+//
+//    public StoreReviewDTO() {
+//    }
+//
+//    public StoreReviewDTO(int code, LocalDateTime createdAt, int visitors, long totalExpense, String oneLineReview, int memberCode, int storeCode) {
+//        this.code = code;
+//        this.createdAt = createdAt;
+//        this.visitors = visitors;
+//        this.totalExpense = totalExpense;
+//        this.oneLineReview = oneLineReview;
+//        this.memberCode = memberCode;
+//        this.storeCode = storeCode;
+//    }
+//
+//    public int getCode() {
+//        return code;
+//    }
+//
+//    public void setCode(int code) {
+//        this.code = code;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public int getVisitors() {
+//        return visitors;
+//    }
+//
+//    public void setVisitors(int visitors) {
+//        this.visitors = visitors;
+//    }
+//
+//    public long getTotalExpense() {
+//        return totalExpense;
+//    }
+//
+//    public void setTotalExpense(long totalExpense) {
+//        this.totalExpense = totalExpense;
+//    }
+//
+//    public String getOneLineReview() {
+//        return oneLineReview;
+//    }
+//
+//    public void setOneLineReview(String oneLineReview) {
+//        this.oneLineReview = oneLineReview;
+//    }
+//
+//    public int getMemberCode() {
+//        return memberCode;
+//    }
+//
+//    public void setMemberCode(int memberCode) {
+//        this.memberCode = memberCode;
+//    }
+//
+//    public int getStoreCode() {
+//        return storeCode;
+//    }
+//
+//    public void setStoreCode(int storeCode) {
+//        this.storeCode = storeCode;
+//    }
 }
