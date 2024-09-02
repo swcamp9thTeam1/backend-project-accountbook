ecaa8536155505de8697760490d661d85e3be312
yanghyeonjin <guswls7081@gmail.com>
2024-09-02 19:13:41 +0900

    FEATURE: 나머지 도메인을 담는 main-server 폴더 추가

---
+++ main-server/src/main/java/com/iiiiii/accountbook/regular_expense/query/service/RegularExpenseService.java
@@ -0,0 +1,41 @@
+package com.iiiiii.accountbook.regular_expense.query.service;
+
+import com.iiiiii.accountbook.regular_expense.query.dto.RegularExpenseDTO;
+import com.iiiiii.accountbook.regular_expense.query.dto.ResponseRegularExpenseDTO;
+import com.iiiiii.accountbook.regular_expense.query.repository.RegularExpenseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("queryService")
+public class RegularExpenseService {
+    private RegularExpenseMapper reMapper;
+
+    @Autowired
+    public RegularExpenseService(RegularExpenseMapper reMapper) {
+        this.reMapper = reMapper;
+    }
+
+    // 고정 지출 목록 조회
+    public List<RegularExpenseDTO> findAllRegularExpenses() {
+        List<RegularExpenseDTO> regularExpenses = reMapper.findAllRegularExpenses();
+
+        return regularExpenses;
+    }
+
+    // 회원별 고정 지출 목록 조회
+    public List<ResponseRegularExpenseDTO> findOneMemberRegularExpenses(int memberCode) {
+        List<ResponseRegularExpenseDTO> regularExpenses = reMapper.findOneMemberRegularExpenses(memberCode);
+
+        return regularExpenses;
+
+    }
+
+    // 고정 지출 번호로 조회
+    public RegularExpenseDTO findOneRegularExpenses(int regularExpenseCode) {
+        RegularExpenseDTO result = reMapper.findOneRegularExpense(regularExpenseCode);
+
+        return result;
+    }
+}
