3d743a2abfa1dc0d5afcbcf04832288b711a02ca
siu98 <parancandy@naver.com>
2024-09-03 16:46:37 +0900

    WebSecurity 수정

---
+++ member-server/src/main/java/com/iiiiii/accbooksecurity/member/security/WebSecurity.java
@@ -0,0 +1,85 @@
+package com.iiiiii.accbooksecurity.member.security;
+
+import com.iiiiii.accbooksecurity.member.command.application.service.MemberService;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final MemberService memberService;
+    private final Environment env;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public WebSecurity(BCryptPasswordEncoder bCryptPasswordEncoder,
+                          MemberService memberService,
+                          Environment env,
+                          JwtUtil jwtUtil) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.memberService = memberService;
+        this.env = env;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(memberService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf -> csrf.disable()));
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(memberService)
+                .passwordEncoder(bCryptPasswordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        http.authorizeHttpRequests((authz) ->
+                        authz.requestMatchers(new AntPathRequestMatcher("/members/**", "POST")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/members/**", "GET")).permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "GET").permitAll()
+
+                                .anyRequest().authenticated()
+                )
+
+                /* 설명. authenticationManager 등록(UserDetails를 상속받는 Service 계층 + BCrypt 암호화) */
+                .authenticationManager(authenticationManager)
+
+                /* 설명. session 방식을 사용하지 않음(JWT Token 방식 사용 시 설정할 내용) */
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilter(getAuthenticationFilter(authenticationManager));
+        http.addFilterBefore(new JwtFilter(memberService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    private Filter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new AuthenticationFilter(authenticationManager, memberService, env);
+    }
+}
