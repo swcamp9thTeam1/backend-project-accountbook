c99ec5e55a5b6060e8f5b979c8d30ee618426b53
hoddeok07 <jhj2969@naver.com>
2024-09-03 18:58:12 +0900

    FIX: import 오류 수정

--- main-server/src/main/java/com/iiiiii/accountbook/review/command/domain/repository/StoreReviewRepository.java
+++ main-server/src/main/java/com/iiiiii/accountbook/review/command/domain/repository/StoreReviewRepository.java
@@ -1,6 +1,5 @@
 package com.iiiiii.accountbook.review.command.domain.repository;
 
-import com.iiiiii.accountbook.accbook.command.domain.aggregate.entity.Accbook;
 import com.iiiiii.accountbook.review.command.domain.aggregate.entity.StoreReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
