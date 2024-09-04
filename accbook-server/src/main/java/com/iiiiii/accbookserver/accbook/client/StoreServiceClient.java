014fae44743811d5c6ce4a41062fbf215efad128
Rohdamin <rohdamin3@gmail.com>
2024-09-04 15:57:37 +0900

    CONFIG: FeignClient 설정

--- accbook-server/src/main/java/com/iiiiii/accbookserver/accbook/client/StoreServiceClient.java
+++ accbook-server/src/main/java/com/iiiiii/accbookserver/accbook/client/StoreServiceClient.java
@@ -2,10 +2,11 @@ package com.iiiiii.accbookserver.accbook.client;
 
 import com.iiiiii.accbookserver.accbook.command.domain.aggregate.vo.RequestRegistStoreVO;
 import com.iiiiii.accbookserver.accbook.command.domain.aggregate.vo.ResponseStoreCodeVO;
+import com.iiiiii.accbookserver.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name="stores", url="localhost:8000")
+@FeignClient(name="stores", url="localhost:8000", configuration = FeignClientConfig.class)
 public interface StoreServiceClient {
     @GetMapping("/main-service/stores/code")
     ResponseStoreCodeVO getStoreCodeByLatLng(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude);
